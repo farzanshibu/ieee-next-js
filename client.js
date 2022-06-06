9e60a120bd9c9ce30ed2faae43edded3e897b67c
Farzan Shibu <69754165+farzanshibu@users.noreply.github.com>
2022-06-06 12:06:03 +0000

    database update

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 import sanityClient from "@sanity/client";
 
 const Client = sanityClient({
-	projectId: "d19epmzn", // you can find this in sanity.json
+	projectId: "5rt0wm1l", // you can find this in sanity.json
 	dataset: "production", // or the name you chose in step 1
 	useCdn: true, // `false` if you want to ensure fresh data
 });
